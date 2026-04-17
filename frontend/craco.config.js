165a812f3559092062a7409e293eac044ae08e2b
Mahammad881 <mh5676023@gmail.com>
2026-04-17 22:25:08 +0530

    Initial commit

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,15 +1,29 @@
-// craco.config.js
+// frontend/craco.config.js
+const webpack = require('webpack');
+
 module.exports = {
   webpack: {
     configure: (webpackConfig) => {
       webpackConfig.resolve = webpackConfig.resolve || {};
+
       webpackConfig.resolve.fallback = {
         ...(webpackConfig.resolve.fallback || {}),
         fs: false,
         path: false,
         os: false,
-        util: false
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        util: require.resolve('util/'),
+        buffer: require.resolve('buffer/')
       };
+
+      webpackConfig.plugins = (webpackConfig.plugins || []).concat([
+        new webpack.ProvidePlugin({
+          process: 'process/browser',
+          Buffer: ['buffer', 'Buffer'],
+        }),
+      ]);
+
       return webpackConfig;
     },
   },
